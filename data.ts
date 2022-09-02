802f4bb91cd01dcee37c9c75c877b245741d006a
Elona Kajtazi <MY_NAME@example.com>
2022-09-02 13:42:47 +0200

    created the data

---
+++ data.ts
@@ -0,0 +1,83 @@
+const houses = [
+  {
+    id: 1,
+    address: "211 Greenrose St. Hopkinsville, KY 42240",
+    type: "farm",
+  },
+  {
+    id: 2,
+    address: "213 Airport Lane Shelbyville, TN 37160",
+    type: "house",
+  },
+  {
+    id: 3,
+    address: "42 Lafayette Road Cincinnati, OH 45211",
+    type: "flat",
+  },
+];
+const residents = [
+  {
+    id: 1,
+    name: "Rory",
+    age: 26,
+    gender: "female",
+    houseId: 1,
+  },
+  {
+    id: 2,
+    name: "Nolan",
+    age: 25,
+    gender: "male",
+    houseId: 1,
+  },
+  {
+    id: 3,
+    name: "Garth",
+    age: 4,
+    gender: "male",
+    houseId: 1,
+  },
+
+  {
+    id: 4,
+    name: "Gabby",
+    age: 14,
+    gender: "female",
+    houseId: 2,
+  },
+  {
+    id: 5,
+    name: "Shena",
+    age: 38,
+    gender: "female",
+    houseId: 2,
+  },
+  {
+    id: 6,
+    name: "Jackson",
+    age: 42,
+    gender: "male",
+    houseId: 2,
+  },
+  {
+    id: 7,
+    name: "Elyse",
+    age: 2,
+    gender: "female",
+    houseId: 1,
+  },
+  {
+    id: 8,
+    name: "Tricia",
+    age: 30,
+    gender: "female",
+    houseId: 3,
+  },
+  {
+    id: 9,
+    name: "Frank",
+    age: 33,
+    gender: "male",
+    houseId: 1,
+  },
+];
