8c313513181e54583a7aea1dcd4d3ea9feb45910
Elona Kajtazi <MY_NAME@example.com>
2022-09-02 11:31:13 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express()
+const port = 5000
+
+app.get ("/", (req, res) => {
+    res.send("Let's start this residents task")
+
+})
+
+app.listen(port, () => {
+    console.log(`App listenin on port ${port}`)
+})
