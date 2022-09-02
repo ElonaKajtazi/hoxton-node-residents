9647c3b869d69d08fb2cba8b4bb866f2785a4ea9
Elona Kajtazi <MY_NAME@example.com>
2022-09-02 16:50:54 +0200

    Patch (update) done!

--- index.ts
+++ index.ts
@@ -52,7 +52,74 @@ app.post("/houses", (req, res) => {
     res.status(400).send({ errors });
   }
 });
+app.post("/residents", (req, res) => {
+  let errors: string[] = [];
+  if (typeof req.body.name !== "string") {
+    errors.push("Name not provided or not a string");
+  }
+  if (typeof req.body.age !== "number") {
+    errors.push("Age not provided or not a number");
+  }
+  if (typeof req.body.gender !== "string") {
+    errors.push("Gender not provided or not a string");
+  }
+  if (typeof req.body.houseId !== "number") {
+    errors.push("HouseId not provided or not a number");
+  }
+  if (errors.length === 0) {
+    let newResident = {
+      id: residents.length === 0 ? 1 : residents[residents.length - 1].id + 1,
+      name: req.body.name,
+      age: req.body.age,
+      gender: req.body.gender,
+      houseId: req.body.houseId,
+    };
 
+    residents.push(newResident);
+    res.send(newResident);
+  } else {
+    res.status(400).send(errors);
+  }
+});
+
+app.patch("/houses/:id", (req, res) => {
+  const id = Number(req.params.id);
+  let match = houses.find((house) => house.id === id);
+
+  if (match) {
+    if (req.body.address) {
+      match.address = req.body.address;
+    }
+    if (req.body.type) {
+      match.type = req.body.type;
+    }
+    res.send(match);
+  } else {
+    res.status(404).send({ error: "House not found!" });
+  }
+});
+
+app.patch("/residents/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const match = residents.find((resident) => resident.id === id);
+  if (match) {
+    if (req.body.name) {
+      match.name = req.body.name;
+    }
+    if (req.body.age) {
+      match.age = req.body.age;
+    }
+    if (req.body.gender) {
+      match.gender = req.body.gender;
+    }
+    if (req.body.houseId) {
+      match.houseId = req.body.houseId;
+    }
+    res.send(match);
+  } else {
+    res.status(404).send({ error: "Resident not found" });
+  }
+});
 app.listen(port, () => {
   console.log(`App listenin on port ${port}`);
 });
